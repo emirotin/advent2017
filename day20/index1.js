d6f45fe48d87307fc71d72c4142fe76226420ce4
Eugene Mirotin <emirotin@gmail.com>
2017-12-20 11:32:50 +0300

    day20 fix part 1

--- day20/index1.js
+++ day20/index1.js
@@ -1,16 +1,23 @@
-const { readLines, parseInt } = require("../lib");
-
-const sum = a => a.reduce((x, y) => x + y, 0);
+const { readLines, parseInt, sum, log } = require("../lib");
 
 const parse = s => {
-  const m = s.match(/^p=<(.*?)>, v=<(.*?)>, a=<(.*?)>$/);
-
-  return [m[3], m[2], m[1]].map(c => sum(c.split(",").map(parseInt)));
+  const m = s.match(/^p=<(.*)>, v=<(.*)>, a=<(.*)>$/);
+  if (!m) {
+    throw new Error("WTF");
+  }
+  const [a, v, p] = [m[3], m[2], m[1]].map(c =>
+    sum(
+      c
+        .split(",")
+        .map(parseInt)
+        .map(Math.abs)
+    )
+  );
+  return [a / 2, v + a / 2, p];
 };
 
 const lines = readLines("./input")
   .map(parse)
-  .map(([a, v, p]) => [a / 2, v + a / 2, p].map(Math.abs))
   .map((el, i) => [...el, i]);
 
 const cmpA = (x, y) => {
@@ -26,7 +33,5 @@ const cmpA = (x, y) => {
 };
 
 lines.sort(cmpA);
-
-console.log(lines.slice(0, 15));
 
 console.log(lines[0][3]);
