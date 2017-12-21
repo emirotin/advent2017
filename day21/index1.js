fff83b7f04c9c136cd8f411cd0aafeee94f7c559
Eugene Mirotin <emirotin@gmail.com>
2017-12-21 10:30:43 +0300

    day21 primitives setup

---
+++ day21/index1.js
@@ -0,0 +1,78 @@
+const { readLines } = require("../lib");
+const range = require("lodash/range");
+const flatten = require("lodash/flatten");
+
+const rules = readLines("./input")
+  .map(s => s.split(" => "))
+  .reduce((acc, [l, r]) => {
+    acc[l] = r;
+    return acc;
+  }, {});
+
+const gridToSubgrids = (grid, subgridSize) => {
+  const size = grid.length;
+  const c = size / subgridSize;
+  return range(0, c).map(i =>
+    range(0, c).map(j =>
+      grid
+        .slice(i * subgridSize, (i + 1) * subgridSize)
+        .map(row => row.slice(j * subgridSize, (j + 1) * subgridSize))
+    )
+  );
+};
+
+const concat = r => r.reduce((acc, r) => acc.concat(r), []);
+
+const subgridsToGrid = ss => {
+  const c = ss.length;
+  const subgridSize = ss[0][0].length;
+  const size = c * subgridSize;
+  return range(0, size).map(i =>
+    range(0, size).map(j => {
+      const x = Math.floor(i / subgridSize);
+      const ii = i % subgridSize;
+      const y = Math.floor(j / subgridSize);
+      const jj = j % subgridSize;
+      return ss[x][y][ii][jj];
+    })
+  );
+};
+
+const gridVariations = g => {
+  const fv = g.slice().reverse();
+  const fh = g.map(r => r.slice().reverse());
+  const c = g.length;
+  const rr = range(0, c).map(i => range(0, c).map(j => g[c - 1 - j][i]));
+  const rl = range(0, c).map(i => range(0, c).map(j => g[j][c - 1 - i]));
+
+  return [g, fv, fh, rl, rr];
+};
+
+const mapSubgrid = g => {
+  const ggs = gridVariations(g).map(gridToString);
+  for (const gg of ggs) {
+    if (rules[gg]) {
+      return stringToGrid(rules[gg]);
+    }
+  }
+  throw new Error(`No rewrite rule for ${gridToString(g)}`);
+};
+
+const ITER = 2;
+
+const gridToString = grid => grid.map(row => row.join("")).join("/");
+
+const stringToGrid = s => s.split("/").map(l => l.split(""));
+
+let grid = stringToGrid(".#./..#/###");
+
+for (let i = 0; i < ITER; i++) {
+  const size = grid.length;
+  const subgridSize = size % 2 ? 3 : 2;
+  const ss = gridToSubgrids(grid, subgridSize).map(rr => rr.map(mapSubgrid));
+  grid = subgridsToGrid(ss);
+}
+
+const logGrid = g => console.log(g.map(r => r.join("")).join("\n"));
+
+logGrid(grid);
