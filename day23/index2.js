c41d1051020f1aaf41572d8e30a02fdc05a28cd3
Eugene Mirotin <emirotin@gmail.com>
2017-12-23 08:21:24 +0300

    day23 part 1 + part 2 stub with the loop

---
+++ day23/index2.js
@@ -0,0 +1,40 @@
+const { readLines, parseInt } = require("../lib");
+
+const parse = line => line.split(" ");
+
+const instructions = readLines("./input").map(parse);
+
+const regs = { a: 1 };
+
+const get = reg => {
+  if (reg.match(/^-?\d+$/)) {
+    return parseInt(reg);
+  }
+  return regs[reg] || 0;
+};
+
+let i = 0;
+while (i >= 0 && i < instructions.length) {
+  console.log("> ", i);
+  const _ = instructions[i];
+  switch (_[0]) {
+    case "set":
+      regs[_[1]] = get(_[2]);
+      break;
+    case "sub":
+      regs[_[1]] = get(_[1]) - get(_[2]);
+      break;
+    case "mul":
+      regs[_[1]] = get(_[1]) * get(_[2]);
+      break;
+    case "jnz":
+      if (get(_[1]) !== 0) {
+        i += get(_[2]);
+        continue;
+      }
+      break;
+  }
+  i += 1;
+}
+
+console.log(regs.h);
