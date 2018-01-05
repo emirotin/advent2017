8107e69142718043d99c3556a15b089f85cb4527
Eugene Mirotin <emirotin@gmail.com>
2018-01-05 17:13:24 +0300

    cleanup

--- clojure/day09/index2.clj
+++ clojure/day09/index2.clj
@@ -25,32 +25,3 @@
 ))
 
 (println total)
-; let total = 0;
-
-; let i = 0;
-; const l = input.length;
-; let inGarbage = false;
-; while (i < l) {
-;   const char = input[i];
-;   switch (char) {
-;     case "<":
-;       if (inGarbage) {
-;         total++;
-;       }
-;       inGarbage = true;
-;       break;
-;     case "!":
-;       i++;
-;       break;
-;     case ">":
-;       inGarbage = false;
-;       break;
-;     default:
-;       if (inGarbage) {
-;         total++;
-;       }
-;   }
-;   i++;
-; }
-
-; console.log(total);
